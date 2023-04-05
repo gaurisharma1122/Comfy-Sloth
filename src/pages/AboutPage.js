bacb02f0623650e9433b7613f9f8e8bbecd92542
gaurisharma1122 <gaurisharma1122@gmail.com>
2023-04-05 11:37:55 +0530

    added about page

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,11 +1,23 @@
 import React from 'react'
 import styled from 'styled-components'
+import PageHero from '../components/PageHero'
+import aboutImg from "../assets/hero-bcg.jpeg"
 
 const AboutPage = () => {
   return (
-    <h4>
-      about page
-    </h4>
+    <main>
+      <PageHero title="about"/>
+      <Wrapper className='page section section-center'>
+        <img src={aboutImg} alt="about image" />
+        <article>
+          <div className="title">
+            <h2>Our Story</h2>
+            <div className="underline"></div>
+          </div>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+        </article>
+      </Wrapper>
+    </main>
   )
 }
 
--- src/pages/CheckoutPage.js
+++ src/pages/CheckoutPage.js
@@ -1,8 +1,16 @@
 import React from 'react'
 import styled from 'styled-components'
+import PageHero from '../components/PageHero'
 
 const CheckoutPage = () => {
-    return <h4>checkout page</h4>
+    return (
+      <main>
+        <PageHero title="checkout"/>
+        <Wrapper className='page'>
+          
+        </Wrapper>
+      </main>
+    )
   }
   const Wrapper = styled.div``
   export default CheckoutPage
