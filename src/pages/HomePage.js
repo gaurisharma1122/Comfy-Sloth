9ca71e81965961918826ce9654e89bc98bc46fb6
gaurisharma1122 <gaurisharma1122@gmail.com>
2023-04-05 12:17:25 +0530

    added hero, services and contact ccomponents in home page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import Hero from '../components/Hero'
+import FeaturedProducts from '../components/FeaturedProducts'
+import Services from '../components/Services'
+import Contact from '../components/Contact'
 
 const HomePage = () => {
   return (
-    <h4>
-      Home page
-    </h4>
+    <main>
+      <Hero/>
+      <FeaturedProducts/>
+      <Services/>
+      <Contact/>
+    </main>
   )
 }
 
